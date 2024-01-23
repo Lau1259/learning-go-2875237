f4da202c93b071d6223a9a0848c376105e163405
Lautaro Cuevas <lautaro.cuevas.rodas@gmail.com>
2024-01-23 01:07:38 -0300

    tested string formatting in go

--- practice/main.go
+++ practice/main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 )
 
+const first = "Lautaro"
+
 func main() {
 
 	var aString string = "This is Go!"
 	fmt.Println(aString)
 	fmt.Printf("The variable's type is %T\n", aString)
+
+	fmt.Println("")
+	fmt.Printf("Hi %s! glad to see you learning GO!", first)
 
 }
